2039f78fc736dd768999fc3656a69a4648cd1b00
rjchriiph <jeanroosjehu@gmail.com>
2022-08-06 16:12:00 -0400

    e5 updt

--- enonse_5.py
+++ enonse_5.py
@@ -4,16 +4,20 @@ chn = "5 45 123  12"
 # ap bay (5) * (4+5) * (1+2+3) * (1+2) => 5*9*6*3
 # ki bay total: 810
 
-chn = "55 45 123  12"
+chn = "50 45 123  12"
 
 chn2 = chn.split()
 chn3 = []
 for x in chn2:
     chn3.append(list(x))
 
+v = list()
+for e in chn3:
+    for f in e:
+        s = 0
+        s+=int(f)
+        v.append(s)
     
 
+print(v)
 
-
-print((chn2))
-print((chn3))
