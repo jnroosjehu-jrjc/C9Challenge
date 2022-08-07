dbba674c4186d4f0932d1c00381e69721dbd1d5a
Jean Roos Jehu <jeanroosjehu@gmail.com>
2022-08-07 03:30:17 -0400

    cmtt x

---
+++ enonse_15.py
@@ -0,0 +1,13 @@
+#coding: UTF-8
+
+
+chn = "hiddenXendpass"
+
+a = chn.index("hidden")
+
+b = chn.index("endpass")
+
+print(a)
+print(b)
+
+print(rch)
--- enonse_5.py
+++ enonse_5.py
@@ -1,23 +1,33 @@
 #coding : UTF-8
 
-chn = "5 45 123  12"
+chn = "5 45 123 12"
 # ap bay (5) * (4+5) * (1+2+3) * (1+2) => 5*9*6*3
 # ki bay total: 810
 
 chn = "50 45 123 12"
-chn2 = chn.split()
 
-chn3 = []
-for x in chn2:
-    chn3.append(list(x))
+chn2 = list(chn)
 
-sl = list()
-for e in chn3:
-    s=0
-    for d in e:
-        pass
-        
-        
+chn3 = [int(x) for x in chn2]
+
+# sl = list()
+
+# for k in chn3:
+#     for i in k:
+#         i = int(i)
+#         sl.append(i)
+
+
+
+
+print(chn2)
 
 print(chn3)
+
+
+
+
+
+
+
 
